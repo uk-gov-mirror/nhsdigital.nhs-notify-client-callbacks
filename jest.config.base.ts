0cef9fa782ac6276e81a5858367a8abf2ddbe0f1
Mike Wild <mike.wild5@nhs.net>
2026-02-23 14:20:21 +0000

    Introduce base test config

---
+++ jest.config.base.ts
@@ -0,0 +1,30 @@
+import type { Config } from "jest";
+
+export const baseJestConfig: Config = {
+  preset: "ts-jest",
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "./.reports/unit/coverage",
+  coverageProvider: "v8",
+  coveragePathIgnorePatterns: ["/__tests__/"],
+  transform: { "^.+\\.ts$": "ts-jest" },
+  testPathIgnorePatterns: [".build"],
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+  reporters: [
+    "default",
+    [
+      "jest-html-reporter",
+      {
+        pageTitle: "Test Report",
+        outputPath: "./.reports/unit/test-report.html",
+        includeFailureMsg: true,
+      },
+    ],
+  ],
+};
+
+export const nodeJestConfig: Config = {
+  ...baseJestConfig,
+  testEnvironment: "node",
+  modulePaths: ["<rootDir>/src"],
+};
--- lambdas/client-transform-filter-lambda/jest.config.ts
+++ lambdas/client-transform-filter-lambda/jest.config.ts
@@ -1,20 +1,7 @@
-import type { Config } from "jest";
+import { nodeJestConfig } from "../../jest.config.base";
 
-export const baseJestConfig: Config = {
-  preset: "ts-jest",
-
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "./.reports/unit/coverage",
-
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
-
+export default {
+  ...nodeJestConfig,
   coverageThreshold: {
     global: {
       branches: 50,
@@ -23,41 +10,8 @@ export const baseJestConfig: Config = {
       statements: -50,
     },
   },
-
-  coveragePathIgnorePatterns: ["/__tests__/"],
-  transform: { "^.+\\.ts$": "ts-jest" },
-  testPathIgnorePatterns: [".build"],
-  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-
-  // Use this configuration option to add custom reporters to Jest
-  reporters: [
-    "default",
-    [
-      "jest-html-reporter",
-      {
-        pageTitle: "Test Report",
-        outputPath: "./.reports/unit/test-report.html",
-        includeFailureMsg: true,
-      },
-    ],
-  ],
-
-  // The test environment that will be used for testing
-  testEnvironment: "jsdom",
-};
-
-const utilsJestConfig = {
-  ...baseJestConfig,
-
-  testEnvironment: "node",
-
   coveragePathIgnorePatterns: [
-    ...(baseJestConfig.coveragePathIgnorePatterns ?? []),
+    ...(nodeJestConfig.coveragePathIgnorePatterns ?? []),
     "zod-validators.ts",
   ],
-
-  // Mirror tsconfig's baseUrl: "src" - automatically resolves non-relative imports
-  modulePaths: ["<rootDir>/src"],
 };
-
-export default utilsJestConfig;
