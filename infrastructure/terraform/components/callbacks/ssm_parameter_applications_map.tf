c29d03d2ba00aac7bc670a970ba48f38224330ec
Rhys Cox <rhys.cox6@nhs.net>
2026-03-13 11:59:14 +0000

    CCM-15215 - Adjusted terraform config

--- infrastructure/terraform/components/callbacks/ssm_parameter_applications_map.tf
+++ infrastructure/terraform/components/callbacks/ssm_parameter_applications_map.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "applications_map" {
+resource "aws_ssm_parameter" "applications_map_ephemeral" {
   count = var.deploy_mock_webhook ? 1 : 0
 
   name   = local.applications_map_parameter_name
@@ -8,4 +8,22 @@ resource "aws_ssm_parameter" "applications_map" {
   value = jsonencode({
     "mock-client" = "mock-application-id"
   })
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "applications_map" {
+  count = var.deploy_mock_webhook ? 0 : 1
+
+  name   = local.applications_map_parameter_name
+  type   = "SecureString"
+  key_id = module.kms.key_arn
+
+  value = jsonencode({})
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
--- infrastructure/terraform/components/callbacks/variables.tf
+++ infrastructure/terraform/components/callbacks/variables.tf
@@ -182,5 +182,6 @@ variable "enable_debug_log_bucket" {
 
 variable "applications_map_parameter_name" {
   type        = string
+  default     = null
   description = "SSM Parameter Store path for the clientId-to-applicationId map used for payload signing"
 }
