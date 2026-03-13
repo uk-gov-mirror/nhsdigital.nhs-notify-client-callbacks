504409b2435f0ed119a77289300cf3b4a7872200
Rhys Cox <rhys.cox6@nhs.net>
2026-03-13 13:49:41 +0000

    CCM-15215 - PR feedback

--- infrastructure/terraform/components/callbacks/ssm_parameter_applications_map.tf
+++ infrastructure/terraform/components/callbacks/ssm_parameter_applications_map.tf
@@ -1,27 +1,11 @@
-resource "aws_ssm_parameter" "applications_map_ephemeral" {
-  count = var.deploy_mock_webhook ? 1 : 0
-
-  name   = local.applications_map_parameter_name
-  type   = "SecureString"
-  key_id = module.kms.key_arn
-
-  value = jsonencode({
-    "mock-client" = "mock-application-id"
-  })
-
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
 resource "aws_ssm_parameter" "applications_map" {
-  count = var.deploy_mock_webhook ? 0 : 1
-
   name   = local.applications_map_parameter_name
   type   = "SecureString"
   key_id = module.kms.key_arn
 
-  value = jsonencode({})
+  value = var.deploy_mock_webhook ? jsonencode({
+    "mock-client" = "mock-application-id"
+  }) : jsonencode({})
 
   lifecycle {
     ignore_changes = [value]
